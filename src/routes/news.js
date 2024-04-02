35ae9022b4435c4504a678d779cd64a640ace487
itstvy <VY.NNT4100@sinhvien.hoasen.edu.vn>
2024-04-02 16:20:18 +0700

    MVC 02

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const newsController = require("../app/controllers/NewsController");
 
-router.use("/:slug", newsController.show);
-router.use("/", newsController.index); // cái "/" nên để ở cuối vì nó sẽ match tất cả các route khác trước
+router.get("/:slug", newsController.show);
+router.get("/", newsController.index); // cái "/" nên để ở cuối vì nó sẽ match tất cả các route khác trước
 
 module.exports = router;
--- src/routes/site.js
+++ src/routes/site.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const siteController = require("../app/controllers/SiteController");
 
-router.use("/search", siteController.search);
-router.use("/", siteController.index); // cái "/" nên để ở cuối vì nó sẽ match tất cả các route khác trước
+router.get("/search", siteController.search);
+router.get("/", siteController.index); // cái "/" nên để ở cuối vì nó sẽ match tất cả các route khác trước
 
 module.exports = router;
